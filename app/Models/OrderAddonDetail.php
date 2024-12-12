da83b9d92b9140a092bcf62ec8d48ca35b2c4785
Shimith Mekkilery <shimithmekilery@gmail.com>
2024-12-12 03:16:14 +0300

    adjuste view single order for all users

--- app/Models/OrderAddonDetail.php
+++ app/Models/OrderAddonDetail.php
@@ -6,11 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 class OrderAddonDetail extends Model
 {
     use HasFactory;
-    protected $fillable = ['order_id', 'order_detail_id', 'addon_id', 'addon_name', 'addon_price', 'is_active'];
+    protected $fillable = [
+        'order_id',
+        'order_detail_id',
+        'addon_id',
+        'addon_name',
+        'addon_price',
+        'is_active'   
+    ];
 
-    public function Order()
-    {
-        return $this->belongsTo(Order::class, 'order_id', 'id');
-        $this->belongsTo(Order::class)->onDelete('cascade');
-    }
+public function Order() {
+    return $this->belongsTo(Order::class , 'order_id', 'id');
+}
+
+public function OrderDetails() {
+    return $this->belongsTo(OrderDetails::class , 'order_detail_id', 'id');
+}
+
+
+public function Addons(){  
+    return $this->hasMany(Addon::class, 'id', 'addon_id');
+
+}
 }
--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -9,14 +9,23 @@ class OrderDetails extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $fillable = ['order_id', 'service_id', 'service_name', 'service_price', 'service_quantity', 'service_detail_total', 'color_code', 'deleted_at'];
-    public function order(): BelongsTo
+    protected $fillable = [
+        'order_id',
+        'service_id',
+        'service_name',
+        'service_price',
+        'service_quantity',
+        'service_detail_total',
+        'color_code',
+        'deleted_at'
+    ];
+    public function order() 
     {
-        return $this->belongsTo(Order::class);
-    }
-
+        return $this->belongsTo(Order::class,'order_id','id');
+    }   
     public function Service()
     {
-        return $this->HasMany(Service::class, 'Service_id', 'id');
+        return $this->HasMany(Service::class,'Service_id', 'id');
     }
+
 }
